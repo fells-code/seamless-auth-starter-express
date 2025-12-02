c009228a83796e69c52f3f4e7207727022818d5b
Brandon Corbett <Bccorb@users.noreply.github.com>
2025-12-01 21:59:55 -0500

    fix: fix env name

--- src/middleware/requireUser.ts
+++ src/middleware/requireUser.ts
@@ -44,7 +44,7 @@ export const requireUser = async (
 
       const seamlessUserData = await getSeamlessUser(
         req,
-        process.env.AUTH_SERVER!
+        process.env.AUTH_SERVER_URL!
       );
 
       if (decodedToken.sub !== seamlessUserData.id) {
